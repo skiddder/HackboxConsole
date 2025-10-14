f71a6de526d2610643b1c34489b75f83e3d9e238
Marco Weber <marcoweber@microsoft.com>
2025-10-14 08:36:25 +0200

    simple password generation has parameters

--- iac/createUsers.ps1
+++ iac/createUsers.ps1
@@ -6,7 +6,10 @@ param(
     [ValidateSet("simple","complex")]
     [string]$hackerPasswordStrength="simple",
     [ValidateSet("simple","complex")]
-    [string]$coachPasswordStrength="complex"
+    [string]$coachPasswordStrength="complex",
+    [string[]]$simplePasswordAdjectives = @(),
+    [string[]]$simplePasswordNouns = @()
+
 )
 
 
@@ -17,16 +20,25 @@ $consoleRoot = Split-Path -Parent $scriptPath
 function generatePassword {
     param(
         [ValidateSet("simple","complex")]
-        [string]$strength="simple"
+        [string]$strength="simple",
+        [string[]]$simpleAdjectives = @(),
+        [string[]]$simpleNouns = @()
     )
     if($strength -eq "simple") {
         # simple password
         $length = 8
-        $fruits = @('apple','banana','cherry','grape','kiwi','lemon','mango','nectarine','orange','papaya','quince','raspberry','strawberry','tangerine','watermelon')
-        $adjectives = @('big','small','tasty', 'sweet','sour','fresh','ripe','juicy','delicious','yummy','crisp','zesty','fruity','succulent', 'flavorful')
-
+        if($null -ne $simpleAdjectives -and $simpleAdjectives.Count -gt 0) {
+            $adjectives = $simpleAdjectives
+        } else {
+            $adjectives = @('big','small','tasty', 'sweet','sour','fresh','ripe','juicy','delicious','yummy','crisp','zesty','fruity','succulent', 'flavorful')
+        }
+        if($null -ne $simpleNouns -and $simpleNouns.Count -gt 0) {
+            $fruits = $simpleNouns
+        } else {
+            $fruits = @('apple','banana','cherry','grape','kiwi','lemon','mango','nectarine','orange','papaya','quince','raspberry','strawberry','tangerine','watermelon')
+        }
         # password is adjective + fruit + 2 digit number
-        return ($adjectives | Get-Random) + "-" +($fruits | Get-Random) + "-" + (Get-Random -Minimum 10 -Maximum 99).ToString()
+        return ($adjectives | Get-Random) + "-" + ($fruits | Get-Random) + "-" + (Get-Random -Minimum 10 -Maximum 99).ToString()
 
     } else {
         # complex password
@@ -41,13 +53,13 @@ $users = @()
 for ($i = 1; $i -le $numberOfTenants; $i++) {
     $users += [PSCustomObject]@{
         "username" = ( $baseHackerUsername + $i )
-        "password" = ( generatePassword -strength $hackerPasswordStrength )
+        "password" = ( generatePassword -strength $hackerPasswordStrength -simpleAdjectives $simplePasswordAdjectives -simpleNouns $simplePasswordNouns )
         "role" = "hacker"
         "tenant" = ( "team" + $i )
     }
     $users += [PSCustomObject]@{
         "username" = ( $baseCoachUsername + $i )
-        "password" = ( generatePassword -strength $coachPasswordStrength )
+        "password" = ( generatePassword -strength $coachPasswordStrength -simpleAdjectives $simplePasswordAdjectives -simpleNouns $simplePasswordNouns )
         "role" = "coach"
         "tenant" = ( "team" + $i )
     }
